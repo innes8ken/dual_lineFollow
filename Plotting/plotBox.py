0e25e3f63d5ef9eb29bcbd3108f855f64d1f75b0
Innes <2382785a@student.gla.ac.uk>
2022-12-19 14:41:39 +0000

    Plotting data and name fix for weight distribution

--- Plotting/plotBox.py
+++ Plotting/plotBox.py
@@ -5,6 +5,7 @@ import matplotlib.pylab as plt
 from plotRobotClass import layer as layerClass
 from plotRobotClass import setFileName
 
+
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams.update({'font.size': 7})
 
@@ -14,7 +15,8 @@ ratioYX=4
 my_dpi=300
 plt.close("all")
 
-
+layers1 = '5,3,3'
+layers2 = '5,6,6'
 path1 = 'FCL/learning/Map 1/'
 path2 = 'FCL/learning/Map 2/'
 fail1 = 'De-Railed.txt'
@@ -24,9 +26,12 @@ y=[]
 
 
 for i in range (4,6):
-    for j in range (1,5):
+    for j in np.arange(1,5,0.5):
+        print(j)
+        if (j%1.0==0):
+            j = int(j)
         for k in range (1,5):
-            folder = path1 + 'L'+ str(j) + 'e-' +str(i) + '_(5,3,3)_F5_run' + str(k) +'/'
+            folder = path2 + 'L'+ str(j) + 'e-' +str(i) + '_('+ layers2 +')_F5_run' + str(k) +'/'
             check = os.path.isdir(folder)
             if check:
                 check = os.path.isfile(folder+fail1)
@@ -48,7 +53,7 @@ print(y)
 plt.xticks([1*pow(10,-5), 1*pow(10,-4), 2*pow(10,-4), 3*pow(10,-4), 4*pow(10,-4)])
 plt.tick_params(labelsize=10, pad=10)
 plt.xlabel("Learning Rate ($\mu$)", fontsize =14 )
-plt.ylabel("Time Steps to Success", fontsize =14)
+plt.ylabel("Time Steps to Success (33 Hz)", fontsize =14)
 plt.scatter(x,y,c='black',s=70)
 #plt.plot(x,y)
 # z = np.polyfit(x,y,2)
